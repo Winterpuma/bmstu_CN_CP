81a450a8b58f58220adf1f0deb8d9256a24626ba
Winterpuma <32524078+Winterpuma@users.noreply.github.com>
2020-12-27 15:26:49 +0300

    Add server state to web

--- ServerManager/WebApplication/ClientApp/src/app/servers-info/servers-info.component.ts
+++ ServerManager/WebApplication/ClientApp/src/app/servers-info/servers-info.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Url } from 'url';
+import { TemplateParseResult } from '@angular/compiler';
 
 @Component({
   selector: 'servers-info-data',
@@ -9,6 +10,8 @@ import { Url } from 'url';
 export class ServersInfoComponent {
   private http: HttpClient; //LOL?
   private baseUrl: string; //LOL?
+
+  public test: string = 'hehe';
 
   public servers: ServerInfo[];
   indx = -1;
@@ -23,15 +26,33 @@ export class ServersInfoComponent {
 
   addServer(address: string) {
     var headers = new HttpHeaders({ 'Content-Type': 'application/json; charset=utf-8' });
-    this.http.post(this.baseUrl + 'api/Manager/AddServer', { uri: address }, { headers: headers }).subscribe(result => { }, error => console.error(error));
+    this.http.post(this.baseUrl + 'api/Manager/AddServer', { uri: address }, { headers: headers }).subscribe(result => {
+      this.test = "Успех";
+      this.servers = result as ServerInfo[];
+    }, error => {
+      console.error(error);
+      this.test = "Ошибка"
+    });
   }
 
   deleteServer(address: string) {
     var headers = new HttpHeaders({ 'Content-Type': 'application/json; charset=utf-8' });
-    this.http.post(this.baseUrl + 'api/Manager/DeleteServer', { uri: address }, { headers: headers }).subscribe(result => { }, error => console.error(error));
+    this.http.post(this.baseUrl + 'api/Manager/DeleteServer', { uri: address }, { headers: headers }).subscribe(result => {
+      this.test = "Успех";
+      this.test = result.toString();
+    }, error => {
+      console.error(error);
+      this.test = "Ошибка"
+    });
   }
 
   updServers() {
+    this.http.get<ServerInfo[]>(this.baseUrl + 'api/Manager/GetUpdatedServers').subscribe(result => {
+      this.servers = result;
+    }, error => console.error(error));
+  }
+
+  getServers() {
     this.http.get<ServerInfo[]>(this.baseUrl + 'api/Manager/GetServers').subscribe(result => {
       this.servers = result;
     }, error => console.error(error));
@@ -41,9 +62,16 @@ export class ServersInfoComponent {
 interface ServerInfo {
   name: string;
   uri: string;
-  isBusy: boolean;
+  state: ServerState;
   curTaskNumber: number;
   totalTasksNumber: number;
   persentageDone: number;
+}
+
+enum ServerState {
+  Free,
+  Busy,
+  Down,
+  NoState
 }
 
