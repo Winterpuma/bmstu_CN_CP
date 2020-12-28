98da770b7aa1c48658fbda9b852d7935b75334e8
Winterpuma <32524078+Winterpuma@users.noreply.github.com>
2020-12-28 13:23:32 +0300

    Add comments

--- ServerManager/ServerManager/Manager.cs
+++ ServerManager/ServerManager/Manager.cs
@@ -6,41 +6,56 @@ namespace ServerManager
 {
     public class Manager
     {
-        ConcurrentDictionary<Uri, Server> availableServers;
+        ConcurrentDictionary<Uri, Server> serverPool;
         public int CurRequestIndex { get; private set; }
         Dictionary<int, Server> requestToServer;
 
         public IEnumerable<Server> Servers
         {
-            get => availableServers.Values;
+            get => serverPool.Values;
         }
 
         public Manager()
         {
-            availableServers = new ConcurrentDictionary<Uri, Server>();
+            serverPool = new ConcurrentDictionary<Uri, Server>();
             requestToServer = new Dictionary<int, Server>();
         }
 
+        /// <summary>
+        /// Добавляет сервер в отслеживаемые
+        /// </summary>
+        /// <param name="uri">Адрес сервера</param>
+        /// <returns>Успешность добавления</returns>
         public int AddServer(Uri uri)
         {
-            bool added = availableServers.TryAdd(uri, new Server(uri));
+            bool added = serverPool.TryAdd(uri, new Server(uri));
             if (!added)
                 return -1;
             return 0;
         }
 
+        /// <summary>
+        /// Удаляет сервер из отслеживаемых
+        /// </summary>
+        /// <param name="uri">Адрес сервера</param>
+        /// <returns>Успешность удаления</returns>
         public int DeleteServer(Uri uri)
         {
-            bool removed = availableServers.TryRemove(uri, out Server value);
+            bool removed = serverPool.TryRemove(uri, out Server value);
             if (!removed)
                 return -1;
             return 0;
         }
 
+        /// <summary>
+        /// Отправляет Get запрос первому свободному серверу
+        /// </summary>
+        /// <param name="path">Адрес запроса</param>
+        /// <returns>Индекс запроса для отслеживания</returns>
         public int SendGetRequest(string path) // возможно переделать на asynk
         {
             bool success = false;
-            foreach (Server server in availableServers.Values)
+            foreach (Server server in serverPool.Values)
             {
                 if (server.State == ServerState.Free) // нашли свободный сервер
                 {
@@ -58,14 +73,42 @@ namespace ServerManager
             return CurRequestIndex;
         }
 
-        public string GetRequestState(int requestId) // а что если такого нет???
+        /// <summary>
+        /// Отправляет Get запрос конкретному серверу
+        /// </summary>
+        /// <param name="uri">Адрес сервера</param>
+        /// <param name="path">Адрес запроса</param>
+        /// <returns>Индекс запроса для отслеживания</returns>
+        public int SendGetRequest(Uri uri, string path)
+        {
+            if (serverPool.TryGetValue(uri, out Server server))
+            {
+                CurRequestIndex++;
+                requestToServer[CurRequestIndex] = server; // текущий реквест связываем с сервером
+                server.SendGetRequest(path);
+            }
+            else
+                return -1; // такого сервера нет
+
+            return CurRequestIndex;
+        }
+
+        /// <summary>
+        /// По индексу получить состояние сервера
+        /// </summary>
+        /// <param name="requestId"></param>
+        /// <returns></returns>
+        public string GetRequestState(int requestId)
         {
             return requestToServer[requestId].UpdateServerState();
         }
         
+        /// <summary>
+        /// Обновить все сервера
+        /// </summary>
         public void UpdateAllServers()
         {
-            foreach (Server server in availableServers.Values)
+            foreach (Server server in serverPool.Values)
                 server.UpdateServerState();
         }
     }
